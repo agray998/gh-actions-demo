7ec5f134935b0b33179f27ca8dd1d94664f68d55
agray998 <a.gray.4797@gmail.com>
2022-09-27 11:51:36 +0100

    added files

---
+++ calc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+add() {
+  echo $(($1 + $2))
+}
+
+sub() {
+  echo $(($1 - $2))
+}
+
+mul() {
+  echo $(($1 * $2))
+}
+
+div() {
+  echo $(($1 / $2))
+}
+
+from_file=false
+
+while getopts "f" opt; do
+  case $opt in
+    'f') while read -a file; do\
+           case "${file[1]}" in\
+             '+') add "${file[0]}" "${file[2]}";;
+             '-') sub "${file[0]}" "${file[2]}";;
+             '/') div "${file[0]}" "${file[2]}";;
+             'x') mul "${file[0]}" "${file[2]}";;
+           esac
+         done;\
+         from_file=true;;
+  esac
+done
+
+if [ ${from_file} == false ]; then
+  case $2 in
+    '+') add $1 $3;;
+    '-') sub $1 $3;;
+    '/') div $1 $3;;
+    'x') mul $1 $3;;
+  esac
+fi
+
+
